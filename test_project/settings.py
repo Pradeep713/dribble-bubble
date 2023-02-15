00bcf8fda025ece3475ba2f6e13842ca680828da
Pradeep <pradeepmokara1806@gmail.com>
2023-02-15 15:48:14 +0530

    Changes

--- test_project/settings.py
+++ test_project/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'users.apps.AccountConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
